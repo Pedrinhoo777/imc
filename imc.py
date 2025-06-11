9bc96ecf700da05f564915ac8e6c05d6ed52f757
Pedrinhoo777 <pedrooliveir35@gmail.com>
2025-06-11 15:01:36 -0300

    Update imc.py

---
+++ imc.py
@@ -0,0 +1,17 @@
+peso = float(input("Digite seu peso (kg): "))
+altura = float(input("Digite sua altura (m): "))
+
+imc = peso / ( altura  2)
+
+peso = float(("Digite seu peso (kg):"))
+altura = float("Digite sua altura (m):"))
+
+imc = peso / (altura * altura)
+
+Faixa de IMC	Classificação
+Menor que 18.5	Abaixo do peso
+18.5 a 24.9	Peso normal
+25.0 a 29.9	Sobrepeso
+30.0 a 34.9	Obesidade Grau I
+35.0 a 39.9	Obesidade Grau II
+40.0 ou mais	Obesidade Grau III (mórbida)
